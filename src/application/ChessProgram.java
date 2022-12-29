0aa0310c9d855f224d9851a5d5c8bfe94f66df1d
Luiz Henrique <henriques1637@gmail.com>
2022-12-29 15:34:29 -0300

    Project created

---
+++ src/application/ChessProgram.java
@@ -0,0 +1,16 @@
+package application;
+
+/**
+ *
+ * @author Datafast
+ */
+public class ChessProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
